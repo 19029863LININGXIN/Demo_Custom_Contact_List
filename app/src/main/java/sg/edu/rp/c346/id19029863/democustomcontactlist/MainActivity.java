2d35c8c7805c86f0e3c2a5332ebf1432005df06a
19029863 <19029863@myrp.edu.sg>
2021-08-02 16:25:50 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id19029863/democustomcontactlist/MainActivity.java
@@ -0,0 +1,41 @@
+package sg.edu.rp.c346.id19029863.democustomcontactlist;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView lvContact;
+    ArrayList<Contact> alContactList;
+    CustomAdapter caContact;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        lvContact=findViewById(R.id.listViewContacts);
+        alContactList = new ArrayList<>();
+        Contact item1 = new Contact("Mary",65,1234567,'F');
+        Contact item2 = new Contact("Ken",65,7654321,'M');
+        alContactList.add(item1);
+        alContactList.add(item2);
+        caContact = new CustomAdapter(this, R.layout.row,alContactList);
+        lvContact.setAdapter(caContact);
+        lvContact.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this,DetailActivity.class);
+                Contact contact = alContactList.get(position);
+                intent.putExtra("Contact", contact);
+                startActivity(intent);
+            }
+        });
+    }
+}
